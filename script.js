d9742adb57fa2bfb510ef34ac24466666305dcae
eastbaycsu <130016143+eastbaycsu@users.noreply.github.com>
2026-06-19 13:02:41 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// script.js
-
 const btnEl = document.getElementById("btn");
 const bmiInputEl = document.getElementById("bmi-result");
 const weightConditionEl = document.getElementById("weight-condition");
